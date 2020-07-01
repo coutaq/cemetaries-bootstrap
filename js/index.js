50c5307cce80228b58ca13927a46614adfb1d2f7
Michael Melikov <coutaq@gmail.com>
2020-07-01 22:24:03 +0300

    im pretty much done

--- js/index.js
+++ js/index.js
@@ -33,18 +33,42 @@ function append(cemetery) {
         "<img src=\""+cemetery.image+"\"class=\"card-img-top m-0\" style=\"height: 12rem; width: 100%\">"+
         "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight\">"+cemetery.name+"</div>" +
         "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight mb-auto\">" +
-        "<a class=\"btn btn-primary\" data-toggle=\"modal\" href=\""+cemetery.link+"\" >Текст кнопки</a>" +
+        "<a class=\"btn btn-primary\" data-toggle=\"modal\" href=#"+cemetery.link+" >Текст кнопки</a>" +
         "</div>" +
         "</div>" +
         "</div>");
+    createModals(cemetery)
 }
-function createModals(modal) {
-    $("#developer_note").after(
-        "<p>Test Text</p>"
+function createModals(cemetery) {
+    $("#modals").append(
+        "<div class=\"modal fade\" id="+cemetery.link+" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">"+
+        "  <div class=\"modal-dialog\">"+
+        "<div class=\"modal-content\">"+
+        "<div class=\"modal-header\">"+
+        " <h5 class=\"modal-title\" id=\"exampleModalLabel\">Заказать</h5>"+
+        " <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">"+
+        " <span aria-hidden=\"true\"></span>"+
+         " </button>"+
+        "  </div>"+
+        "  <div class=\"modal-body d-flex justify-content-center border-bottom\">"+
+        "  <img src=\""+cemetery.image+"\" class=\"img-fluid ml-auto\" alt=\"Responsive image\" style=\"width: 25%; height: 20%\">"+
+        "  <h3 class=\"align-self-center pl-2 mr-auto\">"+cemetery.name+"</h3>"+
+        "  </div>"+
+        " <div class=\"modal-body d-flex justify-content-center flex-column\">"+
+        " <h4 class=\"align-self-center mx-auto\">Информация:</h4>"+
+        " <input id=\"info\" type=\"text\" class=\"form-control\" aria-describedby=\"inputGroup-sizing-sm\">"+
+        " </div>"+
+        "  <div class=\"modal-footer\">"+
+        "  <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Закрыть</button>"+
+        "  <button id=\"sendInfo\" type=\"button\" class=\"btn btn-primary\">Отправить</button>"+
+        "  </div>"+
+        " </div>"+
+        " </div>"+
+        "  </div>"
     );
 }
 
-createModals(undefined)
+
 function filter(text, cemetery) {
     return cemetery.includes(text);
 }
