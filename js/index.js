edc23e82cc70fe75715fa5c195cfbe6afa873c91
Michael Melikov <coutaq@gmail.com>
2020-07-01 14:13:28 +0300

    fixed the sizes

--- js/index.js
+++ js/index.js
@@ -20,21 +20,23 @@ function loadCemeteries() {
         .done(function() {
             console.log( "JSON loaded!" );
             for (var i = 0; i < data.length; i++) {
-                var text =  $("#searchBar").text()
-                $("#developer_note").append(
-                    "<div class=\"cards mx-auto mx-sm-auto mx-sm-4 m-4\" style=\"\">" +
-                        "<div class=\"flex-column d-inline-flex justify-content-center bg-light shadow overflow-hidden\" style=\"height: 18rem;width: 20rem;\">" +
-                            "<img src=\""+data[i].image+"\"class=\"card-img-top m-0\" style=\"height: 12rem; width: 100%\">"+
-                            "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight\">"+data[i].name+"</div>" +
-                            "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight mb-auto\">" +
-                                "<a href=\""+data[i].link+" \" class=\"btn btn-primary\" >Текст кнопки</a>" +
-                            "</div>" +
-                        "</div>" +
-                    "</div>");
+                append(data[i])
             }
         });
 }
 loadCemeteries()
+function append(cemetery) {
+    $("#developer_note").append(
+        "<div class=\"cards mx-auto mx-sm-auto mx-sm-4 m-4\" style=\"\">" +
+        "<div class=\"flex-column d-inline-flex justify-content-center bg-light shadow overflow-hidden\" style=\"height: 18rem;width: 20rem;\">" +
+        "<img src=\""+cemetery.image+"\"class=\"card-img-top m-0\" style=\"height: 12rem; width: 100%\">"+
+        "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight\">"+cemetery.name+"</div>" +
+        "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight mb-auto\">" +
+        "<a href=\""+cemetery.link+" \" class=\"btn btn-primary\" >Текст кнопки</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>");
+}
 function filter(text, cemetery) {
     return cemetery.includes(text);
 }
@@ -43,16 +45,7 @@ $(document).ready(function(){
         $("#developer_note").html("");
         for (var i = 0; i < data.length; i++) {
             if(filter($(this).val().toLowerCase(), data[i].name.toLowerCase())){
-                $("#developer_note").append(
-                    "<div class=\"cards mx-auto mx-sm-auto mx-sm-4 m-4\" style=\"\">" +
-                    "<div class=\"flex-column d-inline-flex justify-content-center bg-light shadow\" style=\"min-width: 15rem;\">" +
-                    "<img src=\""+data[i].image+"\"class=\"card-img-top m-0\" style=\"height: 10rem;max-width: 20rem\">"+
-                    "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight\">"+data[i].name+"</div>" +
-                    "<div class=\"p-2 flex-fill flex-grow-1 bd-highlight mb-auto\">" +
-                    "<a href=\""+data[i].link+" \" class=\"btn btn-primary\" >Текст кнопки</a>" +
-                    "</div>" +
-                    "</div>" +
-                    "</div>");
+                append(data[i])
             }
         }
     });
